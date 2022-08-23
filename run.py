701cb8d8298ef917ed02c12204cbef1d35325158
Ivan <afivan@bk.ru>
2022-08-23 15:55:00 +0300

    improved

--- run.py
+++ run.py
@@ -9,29 +9,36 @@ from bot import send_message
 try:
     excel = parse_excel()
 except Exception as e:
-    send_message(text=f'excel-error: {e}')
     excel = []
+    send_message(text=f'excel-error: {e}')
+    
 
 try:
     yandex = parse_yandex()
 except Exception as e:
-    send_message(text=f'yandex-error: {e}')
     yandex = []
+    send_message(text=f'yandex-error: {e}')
+    
 
 try:
     allright = parse_allright()
 except Exception as e:
-    send_message(text=f'allright-error: {e}')
     allright = []
+    send_message(text=f'allright-error: {e}')
+    
 
 schedule = yandex + allright + excel
 schedule.sort()
 
-text = ''
-for lesson in schedule:
-    timestamp = lesson[0]
-    student = lesson[1]
-    utc = datetime.utcfromtimestamp(timestamp)
-    Moscow_time = (utc + timedelta(hours=3)).strftime('%Y-%m-%d %H:%M')
-    text += f'{Moscow_time} {student}\n'
-send_message(text=text)
+
+if schedule != []:
+    text = ''
+    for lesson in schedule:
+        timestamp = lesson[0]
+        student = lesson[1]
+        utc = datetime.utcfromtimestamp(timestamp)
+        Moscow_time = (utc + timedelta(hours=3)).strftime('%Y-%m-%d %H:%M')
+        text += f'{Moscow_time} {student}\n'
+    send_message(text=text)
+else:
+    send_message(text='Ничего не найдено.')
